94808bc6ce4aae63caf338537cf066de00555ae7
Denise Cabrera <cabrera.denisep@gmail.com>
2025-11-02 12:23:22 -0500

    changed file paths

--- pers-port/src/components/projects/proj-data.js
+++ pers-port/src/components/projects/proj-data.js
@@ -1,11 +1,11 @@
 const projects = [
     {"name": "ClosetCollab", 
     "description": "ClosetCollab was created for my CS50 final project. It is a platform in which Yale students can borrow and lend clothes from other students, while simultaneously tracking their interactions.", 
-    "picture": "../../../public/cc.png",
+    "picture": "/cc.png",
     "link": "https://github.com/denise-cbrr/ClosetCollab.git"},
     {"name": "Personal Portfolio",
     "description": "This personal portfolio was created mainly to showcase my projects (currently working on more). It also provides a few facts about me and some of the photographs I took over the years!",
-    "picture": "../../../public/pers-port.png",
+    "picture": "/pers-port.png",
     "link": "https://github.com/denise-cbrr/denise-portfolio.git"}
 ]
 
